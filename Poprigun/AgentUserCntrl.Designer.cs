0a31892ba4da714c2012fa5e81602106d0076b98
Charozyer <73101361+Charozyer@users.noreply.github.com>
2021-12-07 15:13:20 +0300

    Add files via upload

---
+++ Poprigun/AgentUserCntrl.Designer.cs
@@ -0,0 +1,118 @@
+﻿
+namespace Poprigun
+{
+    partial class AgentUserCntrl
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.TypeAgentLbl = new System.Windows.Forms.Label();
+            this.phoneNumLbl = new System.Windows.Forms.Label();
+            this.P = new System.Windows.Forms.Label();
+            this.priorityLbl = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Poprigun.Properties.Resources.picture;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(160, 144);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // TypeAgentLbl
+            // 
+            this.TypeAgentLbl.AutoSize = true;
+            this.TypeAgentLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.TypeAgentLbl.Location = new System.Drawing.Point(169, 33);
+            this.TypeAgentLbl.Name = "TypeAgentLbl";
+            this.TypeAgentLbl.Size = new System.Drawing.Size(195, 18);
+            this.TypeAgentLbl.TabIndex = 1;
+            this.TypeAgentLbl.Text = "Тип | наименование агента";
+            // 
+            // phoneNumLbl
+            // 
+            this.phoneNumLbl.AutoSize = true;
+            this.phoneNumLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.phoneNumLbl.Location = new System.Drawing.Point(169, 51);
+            this.phoneNumLbl.Name = "phoneNumLbl";
+            this.phoneNumLbl.Size = new System.Drawing.Size(129, 18);
+            this.phoneNumLbl.TabIndex = 3;
+            this.phoneNumLbl.Text = "Номер телефона";
+            // 
+            // P
+            // 
+            this.P.AutoSize = true;
+            this.P.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.P.Location = new System.Drawing.Point(169, 69);
+            this.P.Name = "P";
+            this.P.Size = new System.Drawing.Size(90, 18);
+            this.P.TabIndex = 4;
+            this.P.Text = "Приоритет: ";
+            // 
+            // priorityLbl
+            // 
+            this.priorityLbl.AutoSize = true;
+            this.priorityLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.priorityLbl.Location = new System.Drawing.Point(254, 69);
+            this.priorityLbl.Name = "priorityLbl";
+            this.priorityLbl.Size = new System.Drawing.Size(52, 18);
+            this.priorityLbl.TabIndex = 6;
+            this.priorityLbl.Text = "Приор";
+            // 
+            // AgentUserCntrl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.priorityLbl);
+            this.Controls.Add(this.P);
+            this.Controls.Add(this.phoneNumLbl);
+            this.Controls.Add(this.TypeAgentLbl);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "AgentUserCntrl";
+            this.Size = new System.Drawing.Size(693, 150);
+            this.Load += new System.EventHandler(this.AgentUserCntrl_Load);
+            this.Click += new System.EventHandler(this.AgentUserCntrl_Click);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.AgentUserCntrl_MouseClick);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label TypeAgentLbl;
+        private System.Windows.Forms.Label phoneNumLbl;
+        private System.Windows.Forms.Label P;
+        private System.Windows.Forms.Label priorityLbl;
+    }
+}
---
+++ Poprigun/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿
+namespace Poprigun
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.addButton = new System.Windows.Forms.Button();
+            this.RangeLbl = new System.Windows.Forms.Label();
+            this.DownCheck = new System.Windows.Forms.CheckBox();
+            this.FiltrCombo = new System.Windows.Forms.ComboBox();
+            this.SortCombo = new System.Windows.Forms.ComboBox();
+            this.RightBtn = new System.Windows.Forms.Button();
+            this.LeftBtn = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.SearchTxt = new System.Windows.Forms.TextBox();
+            this.searchLabel = new System.Windows.Forms.Label();
+            this.delButton = new System.Windows.Forms.Button();
+            this.agentBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.agentBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button5.Location = new System.Drawing.Point(663, 501);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(25, 40);
+            this.button5.TabIndex = 28;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(634, 501);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(25, 40);
+            this.button4.TabIndex = 27;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(605, 501);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(25, 40);
+            this.button3.TabIndex = 26;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(574, 501);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(25, 40);
+            this.button2.TabIndex = 25;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(548, 501);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(25, 40);
+            this.button1.TabIndex = 24;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(22, 530);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(152, 23);
+            this.addButton.TabIndex = 23;
+            this.addButton.Text = "Добавить Агента";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // RangeLbl
+            // 
+            this.RangeLbl.AutoSize = true;
+            this.RangeLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.RangeLbl.Location = new System.Drawing.Point(200, 501);
+            this.RangeLbl.Name = "RangeLbl";
+            this.RangeLbl.Size = new System.Drawing.Size(234, 17);
+            this.RangeLbl.TabIndex = 21;
+            this.RangeLbl.Text = "Ангенты с ....  по .... (из всего ..... )";
+            // 
+            // DownCheck
+            // 
+            this.DownCheck.AutoSize = true;
+            this.DownCheck.Location = new System.Drawing.Point(332, 10);
+            this.DownCheck.Name = "DownCheck";
+            this.DownCheck.Size = new System.Drawing.Size(75, 17);
+            this.DownCheck.TabIndex = 20;
+            this.DownCheck.Text = "убывание";
+            this.DownCheck.UseVisualStyleBackColor = true;
+            // 
+            // FiltrCombo
+            // 
+            this.FiltrCombo.FormattingEnabled = true;
+            this.FiltrCombo.Items.AddRange(new object[] {
+            "Все типы"});
+            this.FiltrCombo.Location = new System.Drawing.Point(557, 8);
+            this.FiltrCombo.Name = "FiltrCombo";
+            this.FiltrCombo.Size = new System.Drawing.Size(157, 21);
+            this.FiltrCombo.TabIndex = 18;
+            // 
+            // SortCombo
+            // 
+            this.SortCombo.FormattingEnabled = true;
+            this.SortCombo.Items.AddRange(new object[] {
+            "Без сортировки",
+            "Тип агента",
+            "Приоритет",
+            "Название"});
+            this.SortCombo.Location = new System.Drawing.Point(413, 8);
+            this.SortCombo.Name = "SortCombo";
+            this.SortCombo.Size = new System.Drawing.Size(138, 21);
+            this.SortCombo.TabIndex = 17;
+            // 
+            // RightBtn
+            // 
+            this.RightBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.RightBtn.Location = new System.Drawing.Point(691, 501);
+            this.RightBtn.Name = "RightBtn";
+            this.RightBtn.Size = new System.Drawing.Size(23, 40);
+            this.RightBtn.TabIndex = 12;
+            this.RightBtn.Text = ">";
+            this.RightBtn.UseVisualStyleBackColor = true;
+            this.RightBtn.Click += new System.EventHandler(this.RightBtn_Click_1);
+            // 
+            // LeftBtn
+            // 
+            this.LeftBtn.Enabled = false;
+            this.LeftBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LeftBtn.Location = new System.Drawing.Point(519, 501);
+            this.LeftBtn.Name = "LeftBtn";
+            this.LeftBtn.Size = new System.Drawing.Size(23, 40);
+            this.LeftBtn.TabIndex = 13;
+            this.LeftBtn.Text = "<";
+            this.LeftBtn.UseVisualStyleBackColor = true;
+            this.LeftBtn.Click += new System.EventHandler(this.LeftBtn_Click_1);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 35);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(702, 460);
+            this.flowLayoutPanel1.TabIndex = 11;
+            // 
+            // SearchTxt
+            // 
+            this.SearchTxt.Location = new System.Drawing.Point(12, 9);
+            this.SearchTxt.Name = "SearchTxt";
+            this.SearchTxt.Size = new System.Drawing.Size(214, 20);
+            this.SearchTxt.TabIndex = 19;
+            this.SearchTxt.Enter += new System.EventHandler(this.SearchTxt_Enter);
+            this.SearchTxt.Leave += new System.EventHandler(this.SearchTxt_Leave);
+            // 
+            // searchLabel
+            // 
+            this.searchLabel.AutoSize = true;
+            this.searchLabel.BackColor = System.Drawing.SystemColors.Window;
+            this.searchLabel.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.searchLabel.Location = new System.Drawing.Point(15, 12);
+            this.searchLabel.Name = "searchLabel";
+            this.searchLabel.Size = new System.Drawing.Size(109, 13);
+            this.searchLabel.TabIndex = 0;
+            this.searchLabel.Text = "Введите для поиска";
+            // 
+            // delButton
+            // 
+            this.delButton.Location = new System.Drawing.Point(180, 530);
+            this.delButton.Name = "delButton";
+            this.delButton.Size = new System.Drawing.Size(152, 23);
+            this.delButton.TabIndex = 29;
+            this.delButton.Text = "Удалить Агента";
+            this.delButton.UseVisualStyleBackColor = true;
+            this.delButton.Click += new System.EventHandler(this.delButton_Click);
+            // 
+            // agentBindingSource
+            // 
+            this.agentBindingSource.DataSource = typeof(Poprigun.ModelEF.Agent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 573);
+            this.Controls.Add(this.delButton);
+            this.Controls.Add(this.searchLabel);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.RangeLbl);
+            this.Controls.Add(this.DownCheck);
+            this.Controls.Add(this.SearchTxt);
+            this.Controls.Add(this.FiltrCombo);
+            this.Controls.Add(this.SortCombo);
+            this.Controls.Add(this.RightBtn);
+            this.Controls.Add(this.LeftBtn);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Попрыженок";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.agentBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Label RangeLbl;
+        private System.Windows.Forms.CheckBox DownCheck;
+        private System.Windows.Forms.ComboBox FiltrCombo;
+        private System.Windows.Forms.ComboBox SortCombo;
+        private System.Windows.Forms.Button RightBtn;
+        private System.Windows.Forms.Button LeftBtn;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.TextBox SearchTxt;
+        private System.Windows.Forms.Label searchLabel;
+        private System.Windows.Forms.Button delButton;
+        private System.Windows.Forms.BindingSource agentBindingSource;
+    }
+}
+
